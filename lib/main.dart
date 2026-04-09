424fa5c52acd309f060203de3600b2af6a3f45d6
Rupert <s175379@student.uken.krakow.pl>
2026-04-09 17:05:54 +0200

    2 commit, lab4

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,124 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final List<Task> tasks = [
+    Task(
+        title: "Przygotować prezentację",
+        deadline: "jutro",
+        done: false,
+        priority: "wysoki"),
+    Task(
+        title: "Oddać raport z laboratoriów",
+        deadline: "dzisiaj",
+        done: true,
+        priority: "wysoki"),
+    Task(
+        title: "Powtórzyć widgety Flutter",
+        deadline: "w piątek",
+        done: false,
+        priority: "średni"),
+    Task(
+        title: "Napisać notatki do kolokwium",
+        deadline: "w weekend",
+        done: false,
+        priority: "niski"),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    int doneCount = tasks.where((task) => task.done).length;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("KrakFlow"),
         ),
-        body: Center(
+        body: Padding(
+          padding: EdgeInsets.all(16),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
+              Text(
+                "Masz dziś ${tasks.length} zadania ($doneCount wykonane)",
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Dzisiejsze zadania",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+
+              /// LISTA
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+
+                    return TaskCard(
+                      title: task.title,
+                      subtitle:
+                      "termin: ${task.deadline} | priorytet: ${task.priority}",
+                      icon: task.done
+                          ? Icons.check_circle
+                          : Icons.radio_button_unchecked,
+                    );
+                  },
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+/// MODEL DANYCH
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+  final String priority;
+
+  Task({
+    required this.title,
+    required this.deadline,
+    required this.done,
+    required this.priority,
+  });
+}
+
+/// WIDGET KARTY
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 12),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListTile(
+          leading: Icon(icon, color: Colors.blue),
+          title: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(subtitle),
         ),
       ),
     );
